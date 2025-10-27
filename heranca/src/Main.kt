11926d2de681fe33c905e905969033d76e1585b5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-27 11:32:09 -0300

    Exemplo de herança com um construtor parametrizado

--- heranca/src/Main.kt
+++ heranca/src/Main.kt
@@ -10,7 +10,15 @@ class Yorkishire : Dog (){
     }
 }
 
+open class Tigger(val origin: String) {
+    fun sayHello(){
+        println("O tiger da $origin ")
+    }
+}
 
+//Herdando uma classe passando um parametro no construtor
+//lendo como subertigger é um tigre da siberia
+class SuberTigger : Tigger("Siberia")
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,4 +26,7 @@ fun main() {
     //Instanciando o yorkshire
     val dog: Dog = Yorkishire();
     dog.sayHello();
+
+    val tigger: Tigger = SuberTigger()
+    tigger.sayHello()
 }
