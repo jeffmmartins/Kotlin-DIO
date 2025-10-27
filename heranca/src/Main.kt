2c1803a62522aa7d19df0764c7bfe8419cffef15
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-27 11:44:04 -0300

    exemplo de herança passando argumentos do construtor para a superclasse

--- heranca/src/Main.kt
+++ heranca/src/Main.kt
@@ -20,6 +20,14 @@ open class Tigger(val origin: String) {
 //lendo como subertigger é um tigre da siberia
 class SuberTigger : Tigger("Siberia")
 
+open class Leao (val name: String, val origin: String){
+    fun sayHello(){
+        println("O $name é da $origin")
+    }
+}
+
+class Asiatic(name: String) : Leao(name = name, origin = "Africa")
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -29,4 +37,7 @@ fun main() {
 
     val tigger: Tigger = SuberTigger()
     tigger.sayHello()
+
+    val leao: Leao = Asiatic("Adms")
+    leao.sayHello()
 }
