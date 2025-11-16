ba5c80e3cf2baf1215a450d363f7513f47f21c77
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-11-16 10:35:28 -0300

    Feat: Exemplo de funções lambda criado

---
+++ lambda-function/src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    val upperCase1: (String) -> String = {str: String -> str.uppercase()};
+    val upperCase2: (String) -> String = String::uppercase;
+
+    println(upperCase1("Hello"));
+    println(upperCase2("Hello"));
+
+}
